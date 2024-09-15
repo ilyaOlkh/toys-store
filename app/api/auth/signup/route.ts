9f1682237b50e8be1cb252d2110731fef6791177
illya.ol <illya.ol@fernir.co>
2024-09-15 16:54:53 +0300

    fix prerender

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,6 +1,8 @@
 import { AppRouteHandlerFnContext, handleLogin } from '@auth0/nextjs-auth0';
 import { NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: NextRequest, ctx: AppRouteHandlerFnContext) {
     return handleLogin(req, ctx, {
         authorizationParams: {

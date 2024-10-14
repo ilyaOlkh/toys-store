0014ce277c76d1ac4d26cb081db9a447f94ccb5b
illya.ol <illya.ol@fernir.co>
2024-10-14 22:36:53 +0300

    improved modals

--- app/appComponents/appProvider.tsx
+++ app/appComponents/appProvider.tsx
@@ -2,6 +2,7 @@
 
 import { Comfortaa } from "next/font/google";
 import { createTheme, ThemeProvider } from "@mui/material";
+import { Opacity } from "@mui/icons-material";
 
 const comfortaa = Comfortaa({ subsets: ["latin"] });
 
@@ -13,6 +14,63 @@ const theme = createTheme({
     },
     typography: {
         fontFamily: comfortaa.style.fontFamily,
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 640,
+            md: 768,
+            lg: 1200,
+            xl: 1536,
+        },
+    },
+    components: {
+        MuiBackdrop: {
+            styleOverrides: {
+                root: {
+                    backdropFilter: "blur(10px)",
+                    backgroundColor: "rgba(0, 0, 0, 0.5)",
+                },
+            },
+        },
+        MuiDrawer: {
+            styleOverrides: {
+                paper: {
+                    width: "100%",
+                    marginTop: "100px",
+                    boxSizing: "border-box",
+                    backdropFilter: "blur(1rem)",
+                    height: "calc(100% - 100px)",
+                    borderRadius: "16px",
+                    overflowX: "visible",
+                    overflowY: "visible",
+                    "@media (min-width:640px)": {
+                        width: "400px",
+                        marginTop: "0px",
+                        height: "100%",
+                        borderRadius: "0px",
+
+                        "&::before": {
+                            display: "none",
+                        },
+                    },
+                    "&::before": {
+                        content: '""',
+                        position: "absolute",
+                        top: "-10px",
+                        left: "50%",
+                        transform: "translateX(-50%)",
+                        height: "6px",
+                        width: "100px", // Width of the indicator
+                        backgroundColor: "#fff", // Color of the indicator
+                        borderRadius: "3px", // Optional: rounded corners
+                        marginTop: "-3px", // Position it slightly above the drawer
+                        zIndex: 100, // Ensure it appears above other content
+                        opacity: "0.5",
+                    },
+                },
+            },
+        },
     },
     // components: {
     //     MuiButton: {
--- app/components/modals/favoritesModal.tsx
+++ app/components/modals/favoritesModal.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { Drawer, useMediaQuery } from "@mui/material";
+import { Button, Drawer, SwipeableDrawer, useMediaQuery } from "@mui/material";
 import { useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../redux/store";
 import { useAppSelector } from "../../redux/hooks";
@@ -20,49 +20,59 @@ export default function FavoriteModal() {
     const menuOpen = useAppSelector(
         (state: RootState) => state.modal.openModal === "favorites"
     );
-    const isMobile = useMediaQuery("(max-width: 768px)");
+    const isSmallMobile = useMediaQuery("(min-width: 640px)");
     const favoritesState = useAppSelector(
         (state: RootState) => state.favorites
     );
 
     return (
-        <Drawer
-            anchor="right"
+        <SwipeableDrawer
             open={menuOpen}
             onClose={() => dispatch(closeModal())}
-            sx={{
-                width: 400,
-                flexShrink: 0,
-                "& .MuiDrawer-paper": {
-                    width: 400,
-                    boxSizing: "border-box",
-                },
-            }}
-            className="block"
+            anchor={!isSmallMobile ? "bottom" : "right"}
+            disableSwipeToOpen={false}
+            onOpen={() => {}}
         >
-            <div className="flex flex-col max-h-full">
-                <h2 className="text-xl flex px-2 py-5 justify-center items-center w-full border-b">
-                    Ваші Улюблені товари❤️
-                </h2>
-                <div className="flex flex-col p-4 gap-4 flex-auto overflow-auto">
-                    {favoritesState.favorites.map((product) => (
-                        <ProductCardModal
-                            product={product}
-                            favoritesState={favoritesState}
-                            onClick={() => {
-                                dispatch(removeFavorite(product.product_id));
-                            }}
-                        />
-                    ))}
-                    {[...favoritesState.nowPending, ...favoritesState.queue]
-                        .filter((item) => item.type !== "remove")
-                        .map((item) => item.productId)
-                        .map((item) => (
-                            <span>завантаження...</span>
+            <div className="bg-white size-full rounded-t-[1rem] sm:rounded-t-[0rem] relative">
+                <div className="flex flex-col max-h-full">
+                    <h2 className="relative text-xl flex px-14 py-5 justify-center items-center w-full border-b text-center">
+                        Ваші Улюблені товари❤️
+                        <div className="absolute right-0 top-0 h-full rounded-tr-[1rem] overflow-hidden">
+                            <Button
+                                className="h-full"
+                                onClick={() => dispatch(closeModal())}
+                            >
+                                <Image
+                                    src={"/X.svg"}
+                                    alt="cross"
+                                    width={30}
+                                    height={30}
+                                />
+                            </Button>
+                        </div>
+                    </h2>
+                    <div className="flex flex-col p-4 gap-4 flex-auto overflow-auto">
+                        {favoritesState.favorites.map((product) => (
+                            <ProductCardModal
+                                product={product}
+                                favoritesState={favoritesState}
+                                onClick={() => {
+                                    dispatch(
+                                        removeFavorite(product.product_id)
+                                    );
+                                }}
+                            />
                         ))}
+                        {[...favoritesState.nowPending, ...favoritesState.queue]
+                            .filter((item) => item.type !== "remove")
+                            .map((item) => item.productId)
+                            .map((item) => (
+                                <span>завантаження...</span>
+                            ))}
+                    </div>
                 </div>
             </div>
-        </Drawer>
+        </SwipeableDrawer>
     );
 }
 {
